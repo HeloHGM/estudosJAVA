fabc89ba031f5fd96254fe0d8c4c1678eba5233d
HeloHGM <heloisa.gomes@estudante.ifgoiano.edu.br>
2024-02-01 10:41:44 -0300

    peimeira aula

---
+++ aulaPOO/aula01.java
@@ -0,0 +1,10 @@
+package aulaPOO;
+
+public class aula01 {
+    public static void main(String[] args) {
+       System.out.println("Hello Word");
+
+       if(true)System.out.println("nao precisa de{}");
+    }
+    
+}
