d7b21e6bf6f4fab49e1849103b7f8a3de5499b1d
HeloHGM <heloisa.gomes@estudante.ifgoiano.edu.br>
2024-02-01 10:32:45 -0300

    pasta nova de poo

---
+++ aulaPOO/quest01.java
@@ -0,0 +1,28 @@
+package aulaPOO;
+import java.util.Scanner;
+
+public class quest01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float valorET;
+        float valorGA;
+
+        System.out.println("Preço da gasolina: ");
+        valorGA = scan.nextFloat(); 
+        System.out.println("Preço do etanol: ");
+        valorET = scan.nextFloat(); 
+
+        if( valorET/valorGA <= 0.7){
+            System.out.println("Abasteca com etanol! ");
+
+        }else{
+            System.out.println("Abasteca com gasolina! ");
+
+        }
+
+
+        scan.close();
+    }
+    
+}
